5204cf82d37000eb0ffb2c807ddcd209ede2bb1b
nyon <nilanoyon371@gmail.com>
2026-03-03 20:15:40 +0600

    440

--- test_hq.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from app.scrapers.hqporner.scraper import list_videos
-
-async def test():
-    print("Page 1 length:", len(await list_videos("https://hqporner.com/category/Amateur", 1)))
-    print("Page 2 length:", len(await list_videos("https://hqporner.com/category/Amateur", 2)))
-
-asyncio.run(test())
